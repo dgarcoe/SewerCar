ec8d16abc9b6f09ba4bc86b9b9ad1e5e50c97f3b
dgarcoe <dannn86m@gmail.com>
2019-06-29 21:48:05 +0200

    Added car and sewer entities

---
+++ core/src/com/dgarcoe/sewercar/entities/Car.kt
@@ -0,0 +1,13 @@
+package com.dgarcoe.sewercar.entities
+
+import com.badlogic.gdx.math.Vector2
+
+/**
+ * Created by Daniel on 29/06/2019.
+ */
+class Car(position: Vector2) {
+
+    val BOUNDX = 32
+    val BOUNDY = 32
+
+}
