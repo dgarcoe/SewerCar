cc5a161bb72af82cb3dc7651b09bd1a234f12ec5
dgarcoe <dannn86m@gmail.com>
2019-06-29 21:58:52 +0200

    Added player car

---
+++ core/src/com/dgarcoe/sewercar/entities/PlayerCar.kt
@@ -0,0 +1,12 @@
+package com.dgarcoe.sewercar.entities
+
+import com.badlogic.gdx.math.Vector2
+
+/**
+ * Created by Daniel on 29/06/2019.
+ */
+class PlayerCar(position: Vector2) : Car(position) {
+
+    private val score: Long = 0
+
+}
