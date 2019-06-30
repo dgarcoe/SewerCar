b5494bb9b3c042e90bcda2832e861e97a62d4ade
dgarcoe <dannn86m@gmail.com>
2019-06-30 16:37:17 +0200

    Fixed errors in Car and Sewer classes

--- core/src/com/dgarcoe/sewercar/entities/Sewer.kt
+++ core/src/com/dgarcoe/sewercar/entities/Sewer.kt
@@ -1,12 +1,14 @@
 package com.dgarcoe.sewercar.entities
 
 import com.badlogic.gdx.Game
+import com.badlogic.gdx.math.Rectangle
 import com.badlogic.gdx.math.Vector2
 
 /**
  * Created by Daniel on 29/06/2019.
  */
-class Sewer(position: Vector2) : GameObject() {
+class Sewer(position: Vector2, bounds: Rectangle, boundsSize: Vector2) :
+        GameObject(position,bounds, boundsSize) {
 
     val BOUNDX = 32
     val BOUNDY = 32
