daa9acffec7f1f0ef9a855cc7ef64382093ff688
dgarcoe <dannn86m@gmail.com>
2019-06-30 22:54:17 +0200

    Fixed PlayerCar constructor parameters

--- core/src/com/dgarcoe/sewercar/entities/PlayerCar.kt
+++ core/src/com/dgarcoe/sewercar/entities/PlayerCar.kt
@@ -1,11 +1,13 @@
 package com.dgarcoe.sewercar.entities
 
+import com.badlogic.gdx.math.Rectangle
 import com.badlogic.gdx.math.Vector2
 
 /**
  * Created by Daniel on 29/06/2019.
  */
-class PlayerCar(position: Vector2) : Car(position) {
+class PlayerCar(position: Vector2, bounds: Rectangle, boundsSize: Vector2) :
+        Car(position,bounds,boundsSize) {
 
     private val score: Long = 0
 
